02d228b13e6c34b9590e2e037a83b4fb6e9dce81
fcosilva@hotmail.com <estefani.tipantunia542@gmail.com>
2025-07-10 20:41:56 -0500

    Confirmacion de sistema

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '¡Hola, mundo! Bienvenido al chatbot de Tipantuna Estefani.';
+    return '¡Hola, mundo!';
   }
 }
