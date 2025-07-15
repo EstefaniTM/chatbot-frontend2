a3741501ea38c70d1aafb974c595fb7edc8e1744
fcosilva@hotmail.com <estefani.tipantunia542@gmail.com>
2025-07-15 18:32:41 -0500

    agregacion de inventory

--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ChatbotController } from './chatbot.controller';
+import { ApiChatController } from './api-chat.controller';
 import { ChatbotService } from './chatbot.service';
 import { ConversationsModule } from '../conversations/conversations.module';
 import { CargacsvModule } from '../cargacsv/cargacsv.module';
@@ -8,7 +9,7 @@ import { HuggingFaceModule } from '../huggingface/huggingface.module';
 
 @Module({
   imports: [ConversationsModule, CargacsvModule, HuggingFaceModule],
-  controllers: [ChatbotController],
+  controllers: [ChatbotController, ApiChatController],
   providers: [ChatbotService],
 })
 export class ChatbotModule {}
